e1c97ca59213e91a15e527962eb4811437c500b5
Ray <rayzlui@gmail.com>
2019-08-29 22:44:49 -0700

    fix check bug

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -1,18 +1,9 @@
 import { BLACK_MOVE, WHITE_MOVE } from '../actions/actionTypes';
-import { rankUpPawn } from '../helperFunctons/pawnRankUp';
-import { copyBoard } from '../helperFunctons/copiesBoard';
-import { isCheck, isCheckMate } from '../helperFunctons/checkFunctions';
 import {
   startingPieceLocations,
   setupPlayingBoard,
 } from '../helperFunctons/setupStart';
-import {
-  isCastleMove,
-  castleMove,
-  updatePieces,
-  movePieceOnBoard,
-  removeOpponentPiece,
-} from './reducerFunctions';
+import { runMove } from './reducerFunctions';
 
 const initialState = {
   board: setupPlayingBoard(),
@@ -24,91 +15,15 @@ const initialState = {
 };
 
 export function boardReducer(state = initialState, action) {
-  const { previous, target } = action;
-  let blackPieces = state.blackPieces.slice();
-  let whitePieces = state.whitePieces.slice();
-  let board = copyBoard(state.board);
-  let updatedEnpassant = null;
-  let check = state.check;
-  let checkMate = state.checkMate;
-  let enpassant = state.enpassant;
+  let stateUpdates;
   switch (action.type) {
     case WHITE_MOVE:
-      if (isCastleMove(board, previous, target, 'white')) {
-        board = castleMove(board, whitePieces, previous, target);
-      } else {
-        let piece = board[previous].piece;
-        let copyCurrentPiece = Object.assign({}, piece);
-        if (copyCurrentPiece.castle) {
-          copyCurrentPiece.castle = false;
-        }
-        if (copyCurrentPiece.name === 'Pawn') {
-          copyCurrentPiece = rankUpPawn(copyCurrentPiece, target, board);
-          if (target === enpassant - 8) {
-            updatePieces(blackPieces, enpassant);
-            board[enpassant].piece = null;
-          }
-          if (Math.abs(previous - target) === 16) {
-            updatedEnpassant = target;
-          }
-        }
-        updatePieces(whitePieces, previous, target);
-        removeOpponentPiece(blackPieces, target);
-        movePieceOnBoard(board, previous, target);
-      }
-      if (isCheck(whitePieces, board)) {
-        check = true;
-        if (isCheckMate(whitePieces, board, blackPieces)) {
-          checkMate = true;
-        }
-      }
-      return Object.assign({}, state, {
-        board: board,
-        enpassant: updatedEnpassant,
-        whitePieces: whitePieces,
-        blackPieces: blackPieces,
-        check: check,
-        checkMate: checkMate,
-      });
+      stateUpdates = runMove(state, action, 'white');
+      return Object.assign({}, state, stateUpdates);
 
     case BLACK_MOVE:
-      if (isCastleMove(board, previous, target, 'black')) {
-        board = castleMove(board, blackPieces, previous, target);
-      } else {
-        let piece = board[previous].piece;
-        let copyCurrentPiece = Object.assign({}, piece);
-        if (copyCurrentPiece.castle) {
-          copyCurrentPiece.castle = false;
-        }
-
-        if (copyCurrentPiece.name === 'Pawn') {
-          if (target === enpassant + 8) {
-            updatePieces(whitePieces, enpassant);
-            board[enpassant].piece = null;
-          }
-          copyCurrentPiece = rankUpPawn(copyCurrentPiece, target, board);
-          if (Math.abs(previous - target) === 16) {
-            updatedEnpassant = target;
-          }
-        }
-        updatePieces(blackPieces, previous, target);
-        removeOpponentPiece(whitePieces, target);
-        movePieceOnBoard(board, previous, target);
-      }
-      if (isCheck(blackPieces, board)) {
-        check = true;
-        if (isCheckMate(blackPieces, board, whitePieces)) {
-          checkMate = true;
-        }
-      }
-      return Object.assign({}, state, {
-        board: board,
-        enpassant: updatedEnpassant,
-        whitePieces: whitePieces,
-        blackPieces: blackPieces,
-        check: check,
-        checkMate: checkMate,
-      });
+      stateUpdates = runMove(state, action, 'black');
+      return Object.assign({}, state, stateUpdates);
 
     default:
       return state;
--- src/reducers/reducerFunctions.js
+++ src/reducers/reducerFunctions.js
@@ -1,6 +1,11 @@
+import { copyBoard } from '../helperFunctons/copiesBoard';
+import { rankUpPawn } from '../helperFunctons/pawnRankUp';
+import { isCheck, isCheckMate } from '../helperFunctons/checkFunctions';
+
 export function isCastleMove(board, previous, target, color) {
   let shouldBeKing = board[previous].piece;
   let shouldBeRook = board[target].piece;
+  if (shouldBeKing === null || shouldBeRook === null) return false;
   if (color === 'white') {
     return (
       shouldBeKing.castle &&
@@ -22,16 +27,20 @@ export function castleMove(board, pieces, previous, target) {
   let rooksLanding = target + isLeft * 2;
   board[previous].piece.castle = false;
   board[target].piece.castle = false;
-  movePieceOnBoard(board, previous, kingsLanding);
-  movePieceOnBoard(board, target, rooksLanding);
-  updatePieces(pieces, previous, kingsLanding);
-  updatePieces(pieces, target, rooksLanding);
+  board = movePieceOnBoard(board, previous, kingsLanding);
+  board = movePieceOnBoard(board, target, rooksLanding);
+  pieces = updatePieces(pieces, previous, kingsLanding);
+  pieces = updatePieces(pieces, target, rooksLanding);
   return board;
 }
 
 export function updatePieces(pieces, previous, target) {
   let indexPrevious = pieces.indexOf(previous);
-  pieces.splice(indexPrevious, 1, target);
+  if (target) {
+    pieces.splice(indexPrevious, 1, target);
+  } else {
+    pieces.splice(indexPrevious, 1);
+  }
   return pieces;
 }
 
@@ -49,4 +58,56 @@ export function removeOpponentPiece(pieces, target) {
     pieces.splice(index, 1);
   }
   return pieces;
+}
+
+export function runMove(state, action, colorMove) {
+  const { previous, target } = action;
+  let blackPieces = state.blackPieces.slice();
+  let whitePieces = state.whitePieces.slice();
+  let board = copyBoard(state.board);
+  let updatedEnpassant = null;
+  let check = state.check;
+  let checkMate = state.checkMate;
+  let enpassant = state.enpassant;
+  let [attacking, defending] =
+    colorMove === 'white'
+      ? [whitePieces, blackPieces]
+      : [blackPieces, whitePieces];
+
+  if (isCastleMove(board, previous, target, 'white')) {
+    board = castleMove(board, attacking, previous, target);
+  } else {
+    let piece = board[previous].piece;
+    let copyCurrentPiece = Object.assign({}, piece);
+    if (copyCurrentPiece.castle) {
+      copyCurrentPiece.castle = false;
+    }
+    if (copyCurrentPiece.name === 'Pawn') {
+      copyCurrentPiece = rankUpPawn(copyCurrentPiece, target, board);
+      if (target === enpassant - 8) {
+        blackPieces = updatePieces(defending, enpassant);
+        board[enpassant].piece = null;
+      }
+      if (Math.abs(previous - target) === 16) {
+        updatedEnpassant = target;
+      }
+    }
+    attacking = updatePieces(attacking, previous, target);
+    defending = removeOpponentPiece(defending, target);
+    board = movePieceOnBoard(board, previous, target);
+  }
+  if (isCheck(attacking, board)) {
+    check = true;
+    if (isCheckMate(attacking, board, defending)) {
+      checkMate = true;
+    }
+  }
+  return {
+    board: board,
+    enpassant: updatedEnpassant,
+    whitePieces: colorMove === 'white' ? attacking : defending,
+    blackPieces: colorMove === 'white' ? defending : attacking,
+    check: check,
+    checkMate: checkMate,
+  };
 }
