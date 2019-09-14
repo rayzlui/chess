c7b3e2369b9ec7c84f7ca9326263344c8ef80df1
Ray <rayzlui@gmail.com>
2019-09-13 20:45:26 -0700

    pieceCreaters test

--- src/helperFunctons/pieceCreaters.js
+++ src/helperFunctons/pieceCreaters.js
@@ -1,50 +1,49 @@
 export function Pawn(color) {
   return {
-    name: "Pawn",
+    name: 'Pawn',
     color: color,
-    image: color === "white" ? "whitepawn" : "blackpawn"
+    image: color === 'white' ? 'whitepawn' : 'blackpawn',
   };
 }
 
 export function King(color) {
   return {
-    name: "King",
+    name: 'King',
     color: color,
-    image: color === "white" ? "whitekin" : "blackkin",
+    image: color === 'white' ? 'whitekin' : 'blackkin',
     castle: true,
-    checked: false
   };
 }
 
 export function Queen(color) {
   return {
-    name: "Queen",
+    name: 'Queen',
     color: color,
-    image: color === "white" ? "whiteque" : "blackque"
+    image: color === 'white' ? 'whiteque' : 'blackque',
   };
 }
 
 export function Rook(color) {
   return {
-    name: "Rook",
+    name: 'Rook',
     color: color,
-    image: color === "white" ? "whiteroo" : "blackroo",
-    castle: true
+    image: color === 'white' ? 'whiteroo' : 'blackroo',
+    castle: true,
   };
 }
 
 export function Bishop(color) {
   return {
-    name: "Bishop",
+    name: 'Bishop',
     color: color,
-    image: color === "white" ? "whitebis" : "blackbis"
+    image: color === 'white' ? 'whitebis' : 'blackbis',
   };
 }
 
 export function Knight(color) {
   return {
-    name: "Knight",
+    name: 'Knight',
     color: color,
-    image: color === "white" ? "whitekniht" : "blackknight"
+    image: color === 'white' ? 'whitekniht' : 'blackknight',
   };
 }
