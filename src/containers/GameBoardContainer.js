50b9c5029ff9b13ed6d976e9a29ce46c5da2fd89
Ray <rayzlui@gmail.com>
2019-10-07 22:25:35 -0700

    rework dispatch

--- src/containers/GameBoardContainer.js
+++ src/containers/GameBoardContainer.js
@@ -1,26 +1,30 @@
-import { connect } from "react-redux";
-import { moveWhite, moveBlack } from "../actions/actions";
-import GameBoard from "../views/GameBoardView";
+import { connect } from 'react-redux';
+import { movePiece, moveWhite, moveBlack } from '../actions/actions';
+import GameBoard from '../views/GameBoardView';
 
 function mapStateToProps(state) {
   return {
     board: state.board.board,
     turn: state.turn,
-    whitePieces: state.board.whitePieces,
-    blackPieces: state.board.blackPieces,
-    enpassant: state.board.enpassant, 
+    opponentPiece:
+      state.turn === 'white'
+        ? state.board.blackPieces
+        : state.board.whitePieces,
+    enpassant: state.board.enpassant,
     check: state.board.check,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    moveWhite: (previous, target) => dispatch(moveWhite(previous, target)),
-    moveBlack: (previous, target) => dispatch(moveBlack(previous, target))
+    movePiece: (previous, target, color) => {
+      dispatch(movePiece(previous, target, color));
+      dispatch(color === 'white' ? moveWhite() : moveBlack());
+    },
   };
 }
 
 export const GameBoardContainer = connect(
   mapStateToProps,
-  mapDispatchToProps
-)(GameBoard); 
+  mapDispatchToProps,
+)(GameBoard);
