f7afec763727c4a4f36f6517a87e1e38e566619a
Ray <rayzlui@gmail.com>
2019-10-07 22:27:29 -0700

    edit reducer to handle one turn action

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -1,4 +1,4 @@
-import { BLACK_MOVE, WHITE_MOVE } from '../actions/actionTypes';
+import { MOVE } from '../actions/actionTypes';
 import {
   startingPieceLocations,
   setupPlayingBoard,
@@ -17,12 +17,8 @@ const initialState = {
 export function boardReducer(state = initialState, action) {
   let stateUpdates;
   switch (action.type) {
-    case WHITE_MOVE:
-      stateUpdates = runMove(state, action, 'white');
-      return Object.assign({}, state, stateUpdates);
-
-    case BLACK_MOVE:
-      stateUpdates = runMove(state, action, 'black');
+    case MOVE:
+      stateUpdates = runMove(state, action);
       return Object.assign({}, state, stateUpdates);
 
     default:
