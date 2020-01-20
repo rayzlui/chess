b4c025e105356f671c13bca8e7e34ab7db41a6dc
Ray <rayzlui@gmail.com>
2020-01-20 15:05:54 -0800

    Updates

---
+++ precache-manifest.34514d16e6df8cc52768b17c33eaa507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a406446d21bb18e0d14d42120accdc7e",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "116610315fd437041078",
+    "url": "/chess/static/css/main.29528b6c.chunk.css"
+  },
+  {
+    "revision": "c31b516b6aaab11d4ab4",
+    "url": "/chess/static/js/2.69931c0c.chunk.js"
+  },
+  {
+    "revision": "116610315fd437041078",
+    "url": "/chess/static/js/main.16f518ad.chunk.js"
+  },
+  {
+    "revision": "50babd35036c5a181da8",
+    "url": "/chess/static/js/runtime~main.083cda61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chess/static/media/logo.5d5d9eef.svg"
+  }
+]);
